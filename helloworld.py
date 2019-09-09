2d12f5bc8be156ba59c9fcb695798bfb20679437
Jianrong Yu <jianrong@umich.edu>
2019-09-09 17:46:00 -0400

    finished1

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+
+print('Jianrong Yu')
+a = random.randint(1,100)
+b = random.randint(1,100)
+result = a + b
+print(a)
+print(b)
+print("Sum = ", result)
+print("Average = ", result/2)
